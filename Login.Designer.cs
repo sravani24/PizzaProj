a95630c9e120f83ec7aa2ac1845a1788cda3d764
sravani <sravani.gundapu280@gmail.com>
2015-12-08 07:45:04 +0530

    Pizza App

---
+++ Login.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Pizza
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.CustBtn = new System.Windows.Forms.Button();
+            this.AdminBtn = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.MailLabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Location = new System.Drawing.Point(200, 128);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(429, 312);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.CustBtn);
+            this.panel3.Controls.Add(this.AdminBtn);
+            this.panel3.Location = new System.Drawing.Point(105, 91);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 100);
+            this.panel3.TabIndex = 3;
+            // 
+            // CustBtn
+            // 
+            this.CustBtn.Location = new System.Drawing.Point(58, 55);
+            this.CustBtn.Name = "CustBtn";
+            this.CustBtn.Size = new System.Drawing.Size(75, 23);
+            this.CustBtn.TabIndex = 1;
+            this.CustBtn.Text = "Customer";
+            this.CustBtn.UseVisualStyleBackColor = true;
+            this.CustBtn.Click += new System.EventHandler(this.CustBtn_Click);
+            // 
+            // AdminBtn
+            // 
+            this.AdminBtn.Location = new System.Drawing.Point(58, 14);
+            this.AdminBtn.Name = "AdminBtn";
+            this.AdminBtn.Size = new System.Drawing.Size(75, 23);
+            this.AdminBtn.TabIndex = 0;
+            this.AdminBtn.Text = "Admin";
+            this.AdminBtn.UseVisualStyleBackColor = true;
+            this.AdminBtn.Click += new System.EventHandler(this.AdminBtn_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel2.Controls.Add(this.MailLabel);
+            this.panel2.Location = new System.Drawing.Point(105, 26);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 32);
+            this.panel2.TabIndex = 2;
+            // 
+            // MailLabel
+            // 
+            this.MailLabel.AutoSize = true;
+            this.MailLabel.Location = new System.Drawing.Point(67, 10);
+            this.MailLabel.Name = "MailLabel";
+            this.MailLabel.Size = new System.Drawing.Size(54, 13);
+            this.MailLabel.TabIndex = 0;
+            this.MailLabel.Text = "Pizza App";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 544);
+            this.Controls.Add(this.panel1);
+            this.Name = "Login";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button CustBtn;
+        private System.Windows.Forms.Button AdminBtn;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label MailLabel;
+
+    }
+}
+
---
+++ Login.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Pizza
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CustBtn_Click(object sender, EventArgs e)
+        {
+            Order obj = new Order();
+            obj.Show();
+            this.Hide();
+        }
+
+        private void AdminBtn_Click(object sender, EventArgs e)
+        {
+            Admin obj = new Admin();
+            obj.Show();
+            this.Hide();
+        }
+    }
+}
